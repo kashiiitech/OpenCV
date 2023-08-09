024d7923bb621836138b9491c329ad59ddb6b873
Kashif Ali <kashiiitech@gmail.com>
2023-08-09 22:57:13 +0500

    initial commit

---
+++ file6.py
@@ -0,0 +1,35 @@
+# Detect Corners in an Image
+
+import numpy as np
+import cv2
+
+img = cv2.imread('assets/chessboard.png')
+img = cv2.resize(img, (0, 0), fx=0.75, fy=0.75)
+gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+# Corner detection algorithm
+
+corners = cv2.goodFeaturesToTrack(gray, 100, 0.01, 10)
+# print(corners) float data type
+corners = np.int0(corners)
+
+for corner in corners:
+    x, y = corner.ravel()  #[[x, y]] -> [x, y]
+    cv2.circle(img, (x, y), 5, (255, 0, 0), -1)
+
+  
+
+  # Drawing lines between the corners
+
+for i in range(len(corners)):
+    for j in range(i+1, len(corners)):
+        corner1 = tuple(corners[i][0])
+        corner2 = tuple(corners[j][0])
+        color = tuple(map(lambda x: int(x), np.random.randint(0, 255, size=3)))
+        cv2.line(img, corner1, corner2, color, 1)
+
+
+
+cv2.imshow('Frame', img)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
